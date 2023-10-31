e5fd71f2ee0c8271a100fe67d53eb3168b583d83
Ali Elayni <39239764+nandatanda@users.noreply.github.com>
2023-10-31 11:57:57 -0400

    Draft readFile() method

--- source/FileManager.java
+++ source/FileManager.java
@@ -1,15 +1,25 @@
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-
 
 public class FileManager {
 
     private String path;
 
     FileManager(String p) {
+        path = p;
+    }
 
-
+    public String readFile() {
+        // Read an entire file's contents into a String
+        StringBuilder sb = new StringBuilder();
+        try (FileReader fr = new FileReader(path)) {
+            int character;
+            while ((character = fr.read()) != -1) {
+                sb.append((char) character);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sb.toString();
     }
 }
