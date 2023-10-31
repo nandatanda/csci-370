18a1e142238b2108eff11ecbf4bfe9c4e986f911
Ali Elayni <39239764+nandatanda@users.noreply.github.com>
2023-10-31 11:25:57 -0400

    Create FileManager.java

---
+++ source/FileManager.java
@@ -0,0 +1,15 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class FileManager {
+
+    private String path;
+
+    FileManager(String p) {
+
+
+    }
+}
