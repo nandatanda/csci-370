d72f27df2a2975eabc411abc0781122c6c03ee43
ArmandSoto <armand.e.soto@gmail.com>
2023-11-11 19:09:58 -0500

    Created DataSet Class. Still needs methods to deal with edge cases and checking

---
+++ source/DataSet.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
+public class DataSet {
+    ArrayList<DataPoint> dataPoints = new ArrayList();
+    String[] features;
+    //boolean array that can be used later for checking
+    boolean used[];
+    int size = 0;
+
+    // Constructor for creating Testing, Validation, & Training
+    DataSet(String[] features, int size){
+        this.features = features;
+        this.size = size;
+        this.used = new boolean[size];
+    }
+
+    // Constructor for creating bootstrapped datasets
+    DataSet(String[] features, ArrayList<DataPoint> data){
+        this.dataPoints = data;
+        this.size = dataPoints.size();
+        this.used = new boolean[size];
+
+    }
+
+    // Constructor for original read
+    DataSet(FileText fileText, char delimiter){
+        String fileAsString = fileText.getText();
+        String[] entries;
+        entries = fileAsString.split("\n");
+        this.features = entries[0].split(String.valueOf(delimiter));
+
+        for(int i = 1; i < entries.length; i++){
+            DataPoint row = new DataPoint(features, entries[i], delimiter);
+            dataPoints.add(row);
+        }
+        this.size = dataPoints.size();
+
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void addEntry(DataPoint dp){
+        dataPoints.add(dp);
+        this.size++;
+    }
+
+
+    public ArrayList<DataSet> split(){
+        // training, testing, validation
+        ArrayList<DataSet> subsets = new ArrayList<>();
+
+        // randomize in O(n) time
+        Collections.shuffle(dataPoints, new Random());
+
+        for(int i = 0; i < 3; i++){
+            subsets.add(new DataSet(features, 0));
+        }
+
+        // loop through all data points and separate
+        for(int i = 0; i < size; i++){
+            int subsetIndex = i % 10;
+            int targetSubset = (subsetIndex < 7) ? 0 : ((subsetIndex < 9) ? 1:2);
+            subsets.get(targetSubset).addEntry(dataPoints.get(i));
+        }
+
+        return subsets;
+
+    }
+
+}
