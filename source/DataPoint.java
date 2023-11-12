26fc0ed5ecf9fc000fa297c6a89578fd67efa2c4
Ali Elayni <39239764+nandatanda@users.noreply.github.com>
2023-11-12 14:24:57 -0500

    Change to HashMap <String, Object>

--- source/DataPoint.java
+++ source/DataPoint.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -10,12 +9,7 @@ public class DataPoint {
     /**
      * The data associated with the data point, stored as feature-value pairs.
      */
-    private final HashMap<String, Boolean> data;
-
-    /**
-     * The title of the data point, typically the first part of the row string in a dataset entry.
-     */
-    private final String title;
+    private final HashMap<String, Object> data;
 
     /**
      * Constructs a {@code DataPoint} object by parsing the provided entry and mapping it to features.
@@ -27,35 +21,24 @@ public class DataPoint {
     public DataPoint(String[] features, String entry, char delimiter) {
         this.data = new HashMap<>();
 
-        // Use StringBuilder to build the title until the delimiter is encountered
-        StringBuilder titleBuilder = new StringBuilder();
-        int i = 0;
-        while (i < entry.length() && entry.charAt(i) != delimiter) {
-            char c = entry.charAt(i);
-            titleBuilder.append(c);
-            i++;
-        }
-        this.title = titleBuilder.toString();
-
-        // Move to the next character after the delimiter
-        i++;
-
-        // Split the remaining entry using the delimiter
-        String[] values = entry.substring(i).split(String.valueOf(delimiter));
+        // Split the entire entry using the delimiter
+        String[] values = entry.split(String.valueOf(delimiter));
 
         // Populate the HashMap using features and corresponding values
         for (int j = 0; j < features.length && j < values.length; j++) {
-            data.put(features[j], Boolean.parseBoolean(values[j]));
-        }
-    }
+            String newFeature = features[j];
+            Object newValue;
 
-    /**
-     * Get the title of the data point.
-     *
-     * @return the title as a String
-     */
-    public String getTitle() {
-        return title;
+            if (j == 0) {
+                // For the first feature (title), directly use the value from the entry string
+                newValue = values[j];
+            } else {
+                // For subsequent features, interpret "1" or "true" as boolean true, and other values as boolean false
+                newValue = values[j].equals("1") || values[j].equalsIgnoreCase("true");
+            }
+
+            data.put(newFeature, newValue);
+        }
     }
 
 
@@ -64,8 +47,25 @@ public class DataPoint {
      *
      * @return a copy of the data as a HashMap<String, Boolean>
      */
-    public HashMap<String, Boolean> getData() {
+    public HashMap<String, Object> getData() {
         return new HashMap<>(data);
+    }
+
+    /**
+     * Get a string representation of the DataPoint object.
+     *
+     * @return a string containing the title and feature-value pairs
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("Title: ").append(title).append("\n");
+
+        for (Map.Entry<String, Object> entry : data.entrySet()) {
+            result.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+        }
+
+        return result.toString();
     }
 
 }
