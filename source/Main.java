761af57cbf570e4f6498f9c8858279855951acc7
Mrrrecar721 <mrrrecar721@gmail.com>
2023-11-14 23:53:04 -0500

    Rich here

--- source/Main.java
+++ source/Main.java
@@ -1,9 +1,14 @@
 import java.io.IOException;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String filePath = "../data/Video_games_esrb_rating.csv";
+        File dataSet = new File(filePath);
+
         char delimiter = ',';
         FileText content = new FileText(args[0]);
+
 
         DataSet superset = new DataSet(content, delimiter);
         System.out.print("\n" + superset.dataPoints.get(1));
