3b0d5aa73535181ff32ee3f96aa9570e2fdf5924
Armand Soto <armand.e.soto@gmail.com>
2023-11-16 15:34:18 -0500

    Revert "Reverted DataPoint DataSet and Main"
    This reverts commit 9cb410280caf05939b98d4310f672ec2cc056cd8.

--- source/Main.java
+++ source/Main.java
@@ -1,12 +1,41 @@
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         char delimiter = ',';
-        FileText content = new FileText(args[0]);
+        FileText content = new FileText("data/Video_games_esrb_rating.csv");
 
         DataSet superset = new DataSet(content, delimiter);
-        System.out.print("\n" + superset.dataPoints.get(1));
+        ArrayList<DataSet> subsets;
+        File dataSetObjectFile = new File("data/DataSet.ser");
+        File subsetFile = new File("data/Subsets.ser");
+        Serialization<DataSet> serializer = new Serialization<DataSet>();
+
+        if (!dataSetObjectFile.exists()){
+            serializer.saveToFile(superset, "data/DataSet.ser");
+            subsets = superset.split();
+            serializer.saveListToFile(subsets, "data/Subsets.ser");
+
+        } else{
+            superset = serializer.loadFromFile("data/DataSet.ser", DataSet.class);
+            subsets = serializer.loadListFromFile("data/Subsets.ser", DataSet.class);
+        }
+
+        DataSet trainingSet = subsets.get(0);
+        System.out.println(trainingSet.dataPoints.get(0).getData().get("title"));
+
+
+
+
+
+
+
+
+        System.out.println();
+
+
     }
 
 }
