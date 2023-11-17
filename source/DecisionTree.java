744fb09a8650a1cc89b9c76b6dea50396d8edf4e
Mrrrecar721 <mrrrecar721@gmail.com>
2023-11-16 23:08:08 -0500

    Added generateBSDS method

--- source/DecisionTree.java
+++ source/DecisionTree.java
@@ -1,19 +1,26 @@
 import java.util.ArrayList;
+import java.util.Random;
 public class DecisionTree {
 
     //Root node of the decision
     private Node<DataPoint> root;
     private DataSet bootStrappedDataSet;
+    private int dSetLength;
     private int maxDepth;
     private int minNumSamples;
     private int votesForDataPoint;
     private ArrayList<String> featuresSelected = new ArrayList<String>();
     private ArrayList<String> baggedFeatures = new ArrayList<String>();
+    private ArrayList<DataPoint> dataPointList = new ArrayList<DataPoint>();
+
 
     //Decision Tree Constructor
-    public DecisionTree(DataSet bootStrappedDataSet, int minNumSamples, int maxDepth)
+    DecisionTree(DataSet dSet, int minNumSamples, int maxDepth)
     {
-        this.bootStrappedDataSet = bootStrappedDataSet;
+       //Generate bootstrap
+        this.dSetLength = dSet.dataPoints.size();
+        //Randomize function
+        this.dataPointList = dSet.dataPoints;
         this.minNumSamples = minNumSamples;
         this.maxDepth = maxDepth;
     }
@@ -24,6 +31,17 @@ public class DecisionTree {
     {
 
     }
+
+    public void generateBootStrapDataSet(DataSet dSet)
+    {
+        //Randomly generate bootstrap dataset
+        Random rand = new Random();
+        for (int i = 0; i < this.dSetLength; i++) {
+            int r = rand.nextInt(this.dSetLength);
+            DataPoint current = new DataPoint();
+        }
+    }
+
 
     public void displayOutput()
     {
