85a64c9b60883f3c5729cbed21c4d476fae9bf9c
Ali Elayni <39239764+nandatanda@users.noreply.github.com>
2023-11-26 11:12:49 -0500

    New Delimiter Options
    - Use a string representation for delimiter in config file
    - Declare LinkedHashMap with generic type
    - Add getters for settings fields
    - Add logic for reading of different delimiters from file

--- source/Initializer.java
+++ source/Initializer.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.util.LinkedHashMap;
 
 public class Initializer {
-    private LinkedHashMap<String, Object> configPairs = new LinkedHashMap<String, Object>();
+    private LinkedHashMap<String, Object> configPairs = new LinkedHashMap<>();
 
     Initializer() throws IOException {
         FileText configText = new FileText("config.csv");
@@ -12,8 +12,36 @@ public class Initializer {
         }
     }
 
-    String trainingDirectory() {
+    public String trainingDirectory() {
         return configPairs.get("trainingDirectory").toString();
     }
 
+    public String testingDirectory() {
+        return configPairs.get("testingDirectory").toString();
+    }
+
+    public int trainingSplit() {
+        return Integer.parseInt(configPairs.get("trainingSplit").toString());
+    }
+
+    public int testingSplit() {
+        return Integer.parseInt(configPairs.get("testingSplit").toString());
+    }
+
+    public int nameIndex() {
+        return Integer.parseInt(configPairs.get("nameIndex").toString());
+    }
+
+    public int ratingIndex() {
+        return Integer.parseInt(configPairs.get("ratingIndex").toString());
+    }
+
+    public String delimiter() {
+        String delimiterName = configPairs.get("delimiter").toString().toLowerCase();
+        return switch (delimiterName) {
+            case "comma" -> ",";
+            case "semicolon" -> ";";
+            default -> delimiterName; // Return the input as is if not recognized
+        };
+    }
 }
