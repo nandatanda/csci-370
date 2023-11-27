ec41b6435b03943d662348dfe0f33f62b5e9a01e
Ali Elayni <39239764+nandatanda@users.noreply.github.com>
2023-11-27 00:03:04 -0500

    Style

--- source/Serialization.java
+++ source/Serialization.java
@@ -1,41 +1,43 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class Serialization<T>{
+public class Serialization<T> {
 
-    public void saveToFile(T objToSave, String filename){
+    public void saveToFile(T objToSave, String filename) {
         save(objToSave, filename);
     }
-    public void saveListToFile(ArrayList<T> objToSave, String filename){
-        save(objToSave, filename);
 
+    public void saveListToFile(ArrayList<T> objToSave, String filename) {
+        save(objToSave, filename);
     }
-    private void save(Object objToSave, String filename){
-        try(ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename))){
+
+    private void save(Object objToSave, String filename) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
             outputStream.writeObject(objToSave);
             System.out.println("Object saved to <" + filename + ">");
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public T loadFromFile(String filename, Class<?> objClass){
+    public T loadFromFile(String filename, Class<?> objClass) {
         return load(filename, objClass);
     }
-    public ArrayList<T> loadListFromFile(String filename, Class<?> objClass){
-        T inputObject = load(filename,ArrayList.class);
-        return  (ArrayList<T>) inputObject;
+
+    public ArrayList<T> loadListFromFile(String filename, Class<?> objClass) {
+        T inputObject = load(filename, ArrayList.class);
+        return (ArrayList<T>) inputObject;
     }
 
-    private T load(String filename, Class<?> objClass){
-        try(ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filename))){
+    private T load(String filename, Class<?> objClass) {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filename))) {
             Object inputObject = inputStream.readObject();
-            if (objClass.isInstance(inputObject)){
+            if (objClass.isInstance(inputObject)) {
                 System.out.println("Object at <" + filename + "> loaded");
                 return (T) inputObject;
             }
 
-        } catch(IOException | ClassNotFoundException e){
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return null;
