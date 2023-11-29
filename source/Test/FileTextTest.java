2819e6240bdfe4d23969906c44a3098d49f2bd66
Mrrrecar721 <mrrrecar721@gmail.com>
2023-11-29 15:20:19 -0500

    Better naming convention

---
+++ source/Test/FileTextTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class FileTextTest {
+
+    @Test
+    void should_GetString_When_GivenInput() {
+//        fail("not yet");
+        //given
+        //when
+        //Then
+        assertNotNull();
+    }
+
+    @Test
+    void should_ReturnArrayOfStrings_GivenInput() {
+        //given
+        //when
+        //Then
+        assertNotNull();
+    }
+}
