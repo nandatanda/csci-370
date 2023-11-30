a4a43f814ae82c3a907b53f9e08413b80520bf1a
Mrrrecar721 <mrrrecar721@gmail.com>
2023-11-29 20:28:08 -0500

    Test resources

---
+++ source/test/FileTextTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class FileTextTest {
+
+    @Test
+    void should_ReadFile_When_FileTextObjectIsCreated() {
+        //given
+        String testFilePath = "source/test/resources/Video_games_esrb_rating_test1.csv";
+        //when
+        Executable executable = () -> new FileText(testFilePath);
+        //Then
+        assertDoesNotThrow(() -> executable, "IO Error");
+    }
+
+    @Test
+    void should_ReturnArrayOfStrings_GivenInput() {
+        //given
+        //when
+        //Then
+//        assertNotNull();
+    }
+}
