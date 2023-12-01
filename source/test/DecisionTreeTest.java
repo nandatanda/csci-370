495d2934fbf39497660edc7a2746141a5104ad46
Mrrrecar721 <mrrrecar721@gmail.com>
2023-12-01 00:18:37 -0500

    Added Mockito to dependencies + Bug fix

--- source/test/DecisionTreeTest.java
+++ source/test/DecisionTreeTest.java
@@ -1,26 +1,38 @@
+import org.mockito.Mock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class DecisionTreeTest {
-
-    private ArrayList<String> targetFeaturesValuesMock;
+    @Mock
+    private UserConfig settingsMock;
+    private DecisionTree treeMock;
+    private ArrayList<String> targetFeaturesValuesMock = new ArrayList<String>();
     private DataSet bootstrappedDataSetMock;
-    private ArrayList<String> baggedFeaturesMock;
+    private ArrayList<String> baggedFeaturesMock = new ArrayList<String>();
     private Node rootMock;
     private int minSampleMock;
     private int maxDepthMock;
 
     @BeforeEach
     void setup(){
+        this.rootMock = mock(Node.class);
+        this.settingsMock = mock(UserConfig.class);
+        this.treeMock = mock(DecisionTree.class);
+        this.bootstrappedDataSetMock = mock(DataSet.class);
 
+
+        this.treeMock = new DecisionTree(bootstrappedDataSetMock, baggedFeaturesMock,settingsMock);
     }
 
     @Test
     void should_BuildTree_When_ObjectIsCreated() {
+        System.out.println("Hello");
     }
 
 }
