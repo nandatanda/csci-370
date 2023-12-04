519f3053eec33193331cfe3dcb6a3548f3ac2721
Ali Elayni <39239764+nandatanda@users.noreply.github.com>
2023-12-04 11:46:25 -0500

    Initialize Fields In Constructor
    - Initialize fields.
    - Add javadocs.

--- source/Node.java
+++ source/Node.java
@@ -1,8 +1,11 @@
-import java.util.ArrayList;
-
+/**
+ * The {@code Node} class represents a node in a decision tree used for classification.
+ * Each node contains information about the data it represents, the splitting feature,
+ * and impurity measures for decision tree construction.
+ */
 public class Node {
 
-    private RatingsMap ratingsCount;
+    private final RatingsMap ratingsCount;
     private String splittingFeature;
     private double giniIndex;
     private double giniImpurity;
@@ -12,41 +15,97 @@ public class Node {
     private Node right;
     private String label;
 
+    /**
+     * Default constructor for a node with an empty dataset.
+     */
     public Node() {
+        ratingsCount = new RatingsMap();
+        ratingsCount.initialize();
+        splittingFeature = "";
+        giniIndex = 0.0;
+        giniImpurity = 0.0;
+
         data = new DataSet();
+        left = null;
+        right = null;
+        label = "";
     }
 
+    /**
+     * Constructor for a node with a specified dataset.
+     *
+     * @param data the dataset associated with the node
+     */
     public Node(DataSet data) {
+        ratingsCount = new RatingsMap();
+        ratingsCount.initialize();
+        splittingFeature = "";
+        giniIndex = 0.0;
+        giniImpurity = 0.0;
+
         this.data = data;
+        left = null;
+        right = null;
+        label = "";
     }
 
-    public Node(String feature, ArrayList<String> ratings) {
-        this.splittingFeature = feature;
-        this.data = new ArrayList<>();
-        this.ratingsCount = new RatingsMap();
-        this.ratingsCount.initialize();
+    /**
+     * Constructor for a node representing a split on a specific feature with associated ratings.
+     *
+     * @param feature the feature on which the split is performed
+     */
+    public Node(String feature) {
+        ratingsCount = new RatingsMap();
+        ratingsCount.initialize();
+        splittingFeature = feature;
+        giniIndex = 0.0;
+        giniImpurity = 0.0;
+
+        this.data = new DataSet();
     }
 
+    /**
+     * Get the dataset associated with the node.
+     *
+     * @return the dataset associated with the node
+     */
     public DataSet data() {
         return data;
     }
 
-    public void add(DataRecord datapoint) {
-        String rating = datapoint.rating();
+    /**
+     * Add a data record to the node's dataset and update ratings count.
+     *
+     * @param record the data record to be added
+     */
+    public void add(DataRecord record) {
+        String rating = record.rating();
         ratingsCount.increment(rating);
-        data.add(datapoint);
-
+        data.add(record);
     }
 
+    /**
+     * Get the splitting feature for the node.
+     *
+     * @return the splitting feature for the node
+     */
     public String splittingFeature() {
         return this.splittingFeature;
     }
 
+    /**
+     * Set the splitting feature for the node.
+     *
+     * @param f the splitting feature to set
+     */
     public void setSplittingFeature(String f) {
         this.splittingFeature = f;
     }
 
-    public void calculateGiniImpurity() {
+    /**
+     * Calculate the impurity of the node using Gini impurity measure.
+     */
+    public void calculateImpurity() {
         double gini = 1, classProbability;
         for (String f : ratingsCount.keySet()) {
             classProbability = (double) ratingsCount.get(f) / data.size();
@@ -55,36 +114,69 @@ public class Node {
         giniImpurity = gini;
     }
 
+    /**
+     * Get the Gini index of the node.
+     *
+     * @return the Gini index of the node
+     */
     public double giniIndex() {
         return giniIndex;
     }
 
+    /**
+     * Get the Gini impurity of the node.
+     *
+     * @return the Gini impurity of the node
+     */
     public double giniImpurity() {
         return giniImpurity;
     }
 
+    /**
+     * Get the left child node.
+     *
+     * @return the left child node
+     */
     public Node left() {
         return left;
     }
 
+    /**
+     * Get the right child node.
+     *
+     * @return the right child node
+     */
     public Node right() {
         return right;
     }
 
+    /**
+     * Check if the node is a leaf node.
+     *
+     * @return true if the node is a leaf node, false otherwise
+     */
     public boolean isLeaf() {
         return (left == null && right == null);
     }
 
+    /**
+     * Get the label assigned to the node.
+     *
+     * @return the label assigned to the node
+     */
     public String label() {
         return this.label;
     }
 
+    /**
+     * Assign a label to the node based on the majority feature in the leaf.
+     */
     public void assignLabel() {
-        //label majority feature in leaf
+        // Label majority feature in leaf
         String majority = "";
         int greatestCount = 0;
         for (String f : ratingsCount.keySet()) {
-            int currentCount = (int) ratingsCount.get(f);
+            int currentCount = ratingsCount.get(f);
             if (currentCount > greatestCount) {
                 greatestCount = currentCount;
                 majority = f;
@@ -92,5 +184,9 @@ public class Node {
         }
 
         label = majority;
+    }
+
+    public void splitBy(String feature) {
+        
     }
 }
